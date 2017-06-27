5b973a80551d8992352dd339d5c0b9129e03c9e9
mauricioobgo <mauricioobgp@github.com>
2017-06-26 22:50:01 -0500

    nuevos cambios

---
+++ firstModel.py
@@ -0,0 +1,12 @@
+from sklearn.neighbors import KNeighborsClassifier
+
+
+X = [[0], [1], [2], [3]]
+y = [0, 0, 1, 1]
+
+
+neigh = KNeighborsClassifier(n_neighbors=3)
+
+neigh.fit(X, y) 
+
+print(neigh.predict_proba([[0.9]]))
