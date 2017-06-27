bb2e3b8c11d900cd7a65a395d0e576f9cde53466
mauricioobgo <mauricioobgp@github.com>
2017-06-26 22:54:05 -0500

    cambios con coentarios de modelo

--- firstModel.py
+++ firstModel.py
@@ -1,12 +1,16 @@
 from sklearn.neighbors import KNeighborsClassifier
 
-
+#Labels 
 X = [[0], [1], [2], [3]]
+#Data according to labels to train
 y = [0, 0, 1, 1]
 
-
+#n_neighbors, me dice cuantos puntos cercanos debería tener para clasificarlo en una categoría
 neigh = KNeighborsClassifier(n_neighbors=3)
 
+#Entrenamiento de modelo
 neigh.fit(X, y) 
 
+
+#Predicción de modelo, con un dato nuevo
 print(neigh.predict_proba([[0.9]]))
